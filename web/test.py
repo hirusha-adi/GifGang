8c70f920aa70ac00f569d2ad71ab2fed8c075277
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-01 16:04:38 +0530

    Update test.py

--- web/test.py
+++ web/test.py
@@ -1,4 +1,4 @@
-x = '''["https://nekos.life/api/hug","https://nekos.life/api/kiss","https://nekos.life/api/lizard","https://nekos.life/api/neko","https://nekos.life/api/pat","https://nekos.life/api/v2/img/slap","https://nekos.life/api/v2/img/cuddle","https://nekos.life/api/v2/img/avatar","https://nekos.life/api/v2/img/poke","https://nekos.life/api/v2/img/feed"]'''
+x = '''['https://nekos.life/api/hug', 'https://nekos.life/api/kiss', 'https://nekos.life/api/lizard', 'https://nekos.life/api/neko', 'https://nekos.life/api/pat', 'https://nekos.life/api/v2/img/slap', 'https://nekos.life/api/v2/img/cuddle', 'https://nekos.life/api/v2/img/avatar', 'https://nekos.life/api/v2/img/poke', 'https://nekos.life/api/v2/img/feed']'''
 y = []
 
 splitted = x.split(",")
@@ -6,7 +6,7 @@ splitted[-1] = splitted[-1][:-1]
 splitted[0] = splitted[0][1:]
 
 for i in splitted:
-    y.append(str(i[1:-1]))
+    y.append(str(i).strip()[1:-1])
 
 for j in y:
     print(j)
