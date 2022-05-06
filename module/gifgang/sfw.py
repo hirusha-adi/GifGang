fb70157f71fbb03b0557121b4180463806620956
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-06 14:26:13 +0530

    completed giphy trending

--- module/gifgang/sfw.py
+++ module/gifgang/sfw.py
@@ -27,18 +27,23 @@ class Giphy:
             offset = rand.randint(1, 4990 - limit)
 
         giphy_url_list = []
-        for _ in range(limit):
-            r = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key={self._api_key}&limit={limit}&offset={offset}")
-            if 300 > r.status_code >= 200:
-                try:
-                    data = r.json()
+
+        r = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key={self._api_key}&limit={limit}&offset={offset}")
+        if 300 > r.status_code >= 200:
+            try:
+                data = r.json()
+                for item in data["data"]:
                     giphy_url_list.append(
                         {
-                            "url": data["data"]["images"]["original"]["url"],
-                            "title": data["data"]["title"]
+                            "url": item["images"]["original"]["url"],
+                            "title": item["title"]
                         }
                     )
-                except Exception as e:
-                    print(e)
+            except Exception as e:
+                print(e)
+
         return giphy_url_list
 
+
+    def search(self):
+        pass
