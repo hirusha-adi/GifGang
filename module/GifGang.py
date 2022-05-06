bef3a38c6f4760df2a3ad025f75c1a548997923e
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-06 22:13:37 +0530

    all SFW funcs demo

--- module/GifGang.py
+++ module/GifGang.py
@@ -30,3 +30,16 @@ obj = sfw.Tenor()
 x = obj.random(limit=5, locale="en_US", ar_range="all", contentfilter="off")
 x = obj.search(query="random", limit=5, locale="en_US",
                ar_range="all", contentfilter="off")
+
+# Cats
+obj = sfw.Cats()
+x = obj.theCatAPI(limit=5, size="med", mime_types="jpg,gif,png",
+                  order="RANDOM", has_breeds=0)
+x = obj.CatsAsAService.image(text=None, size="med", color=None, type=None,
+                             filter=None, width=None, height=None)
+x = obj.CatsAsAService.gif(text=None, size="med", color=None, type=None,
+                           filter=None, width=None, height=None)
+
+# Dogs
+obj = sfw.Dogs()
+x = obj.images(limit=5)
