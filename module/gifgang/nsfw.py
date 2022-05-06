4abf822d8580637c1e37ab8a6eb386fac4dd2154
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-06 20:32:29 +0530

    random query list

--- module/gifgang/nsfw.py
+++ module/gifgang/nsfw.py
@@ -1,3 +1,46 @@
+import requests
+import random as rand
+
+
 class Eporner:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.__random_query_lis = [
+            "amateur",
+            "anal",
+            "asian",
+            "blonde",
+            "bigtits",
+            "blowjob",
+            "creampie",
+            "cumshot",
+            "double penetration",
+            "ebony",
+            "facials",
+            "group",
+            "hentai",
+            "interracial",
+            "latina",
+            "japanese",
+            "lingerie",
+            "lesbian",
+            "masturbation",
+            "mature",
+            "milf",
+            "pov",
+            "public",
+            "redhead",
+            "squirting",
+            "wildcrazy",
+            "teens",
+            "cuckold",
+            "oral",
+            "cumshot facial",
+            "pussy",
+            "pussy licking",
+            "deep throat",
+            "cum on tits",
+            "threesome",
+            "bondage",
+            "bdsm",
+            "celebrity"
+        ]
