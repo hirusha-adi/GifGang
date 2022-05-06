68ad540b444b2a664a870dcaa2df97a18bba12a6
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-06 14:37:29 +0530

    demo

--- module/GifGang.py
+++ module/GifGang.py
@@ -1,6 +1,26 @@
 """
-The official Python API of GifGang
+Basic Usage Demonstration for the GifGang python module
 """
 
+# Importing
+# -----------------------------------------------------
+
+# Import SFW classes
+from gifgang import sfw
+
+# Import NSFW Classes
+from gifgang import sfw
+
+
+# SFW Classes Usage Demonstration
+# -----------------------------------------------------
+
+# Giphy
+x = sfw.Giphy.random(limit=5)
+x = sfw.Giphy.trending(limit=5, offset=5)
+x = sfw.Giphy.search(query="cats", limit=5, offset=5)
+
+# Picsum
+x = sfw.Picsum.images(limit=5, height=500, width=200)
 
 
