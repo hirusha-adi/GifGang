a4c9c2fd15d99e829552c8ac95f5c87fcd897ff4
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-07 23:20:36 +0530

    work with sample data for now

--- web/routes/torrents.py
+++ web/routes/torrents.py
@@ -4,7 +4,24 @@ from utils import WebsiteData, count_total_visits_amount, log, logf
 
 
 def torrents_index():
+    torrents_list = [
+        {
+            "title": "Send Boobies",
+            "size": "6969 MB",
+            "link": "LongAssMagnetURL",
+            "se": 5,
+            "le": 20
+        },
+        {
+            "title": "Send Pussy Cat",
+            "size": "6969 MB",
+            "link": "LongAssMagnetURL",
+            "se": 10,
+            "le": 15
+        },
+    ]
     return render_template(
         "torrents/index.html",
-        show_torrents_index=True
+        show_torrents_index=True,
+        torrents_list=torrents_list
     )
