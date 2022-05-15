a205c1acebd62e9591dec97e1c63210c63fd6768
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-15 19:29:25 +0530

    disabled `on_command_error` event for development

--- discordbot/cogs/Base.py
+++ discordbot/cogs/Base.py
@@ -28,80 +28,80 @@ class Base(commands.Cog):
             )
         )
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
+    # @commands.Cog.listener()
+    # async def on_command_error(self, ctx, error):
 
-        embed = discord.Embed(
-            title="An error has occured",
-            color=0xff0000,
-            timestamp=datetime.utcnow()
-        )
-        embed.set_author(
-            name=str(self.client.user.name),
-            icon_url=str(self.client.user.avatar_url)
-        )
+    #     embed = discord.Embed(
+    #         title="An error has occured",
+    #         color=0xff0000,
+    #         timestamp=datetime.utcnow()
+    #     )
+    #     embed.set_author(
+    #         name=str(self.client.user.name),
+    #         icon_url=str(self.client.user.avatar_url)
+    #     )
 
-        embed.set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/877796755234783273/961984775176982558/unknown.png?size=4096")
+    #     embed.set_thumbnail(
+    #         url="https://cdn.discordapp.com/attachments/877796755234783273/961984775176982558/unknown.png?size=4096")
 
-        if isinstance(error, commands.MissingAnyRole):
-            embed.add_field(
-                name="Error:",
-                value="User Missing Any Role.",
-                inline=False
-            )
+    #     if isinstance(error, commands.MissingAnyRole):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value="User Missing Any Role.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.MissingPermissions):
-            embed.add_field(
-                name="Error:",
-                value="User Missing Permissions to use this command.",
-                inline=False
-            )
+    #     if isinstance(error, commands.MissingPermissions):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value="User Missing Permissions to use this command.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.MissingRequiredArgument):
-            embed.add_field(
-                name="Error:",
-                value="Missing Required Argument. Not all arguments for the usage of this command was passed. Please refer help.",
-                inline=False
-            )
+    #     if isinstance(error, commands.MissingRequiredArgument):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value="Missing Required Argument. Not all arguments for the usage of this command was passed. Please refer help.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.MissingRole):
-            embed.add_field(
-                name="Error:",
-                value="User Missing Role. You dont have a required role to use this command.",
-                inline=False
-            )
+    #     if isinstance(error, commands.MissingRole):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value="User Missing Role. You dont have a required role to use this command.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.BotMissingAnyRole):
-            embed.add_field(
-                name="Error:",
-                value=f"Bot Missing Role. {self.client.user.name} does not have any role for this.",
-                inline=False
-            )
+    #     if isinstance(error, commands.BotMissingAnyRole):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value=f"Bot Missing Role. {self.client.user.name} does not have any role for this.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.BotMissingPermissions):
-            embed.add_field(
-                name="Error:",
-                value=f"Bot Missing Permissions. {self.client.user.name} does not have enough permission for this command to be run successfully.",
-                inline=False
-            )
+    #     if isinstance(error, commands.BotMissingPermissions):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value=f"Bot Missing Permissions. {self.client.user.name} does not have enough permission for this command to be run successfully.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.BotMissingRole):
-            embed.add_field(
-                name="Error:",
-                value=f"Bot Missing Role. {self.client.user.name} does not have a required role to complete this action.",
-                inline=False
-            )
+    #     if isinstance(error, commands.BotMissingRole):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value=f"Bot Missing Role. {self.client.user.name} does not have a required role to complete this action.",
+    #             inline=False
+    #         )
 
-        if isinstance(error, commands.ArgumentParsingError):
-            embed.add_field(
-                name="Error:",
-                value=f"Unable to process the arguments given with the command",
-                inline=False
-            )
+    #     if isinstance(error, commands.ArgumentParsingError):
+    #         embed.add_field(
+    #             name="Error:",
+    #             value=f"Unable to process the arguments given with the command",
+    #             inline=False
+    #         )
 
-        embed.set_footer(text=f"Reuqested by {ctx.author.name}")
-        await ctx.send(embed=embed)
+    #     embed.set_footer(text=f"Reuqested by {ctx.author.name}")
+    #     await ctx.send(embed=embed)
 
     @commands.command()
     async def ping(self, ctx):
