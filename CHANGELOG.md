f230f751171e7b39853737da6ca6bde46eb01194
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-15 18:46:58 +0530

    changed the location of all the folders

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,53 @@
-# Changelog of [Web App](https://github.com/hirusha-adi/GifGang/blob/main/web/CHANGELOG.md)
+# 1.4
+
+- Download torrents with the stored Magnet-URLs in MongoDB
+- get torrents list by filtering from uploaded channel name
+- New search pages for every service
+- backend bug fixes
+- redirect to login page if wrong login information is providied instead of the debug mode taking over
+- completed adding all image URLs for [NSFW Categories List](https://github.com/hirusha-adi/GifGang/issues/8) - Thank you OLIVER!
+
+# 1.3
+
+- Better route management, Clean Code
+- backend Bug Fixes
+- search bar color fix
+- New pin added at first position in the Pinned List in both SFW and NSFW that will redirect to `/links`
+
+# v1.2
+
+## Whats new?
+
+1. Website Only (`./web/*`)
+2. Manage all settings stored in json files with the admin settings page `/admin/settings` (Also has a very nice mobile UI)
+   3, Default Dark Mode
+3. Color theme changes - [New Color Pallete](https://www.color-hex.com/color-palette/97670)
+4. THE DARK MODE IS ADDED ONLY TO PLUBLICLY AVAILABLE ROUTES. ALL ADMIN ROUTES WILL BE STICKING WITH THIER ORIGINAL THEMES
+
+## Bugs
+
+1. Bad text color and background color in Search bar (input-field)
+
+# v1.1
+
+1.  Website Only
+2.  Display the current request count given to the web app at the bottom
+3.  added META tags for the website
+4.  Log to file
+
+```
+[time] (IP-Address) - /webpage - "User Agent (Browser Info)"
+```
+
+```
+[2022-04-24 21:25:41.526415] (127.0.0.1) - /about - "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36"
+```
+
+5.  Better Code. Manage services with functions outside from the main file (in `services.*`)
+6.  Change in privacy policy for better protection of users
+7.  Secure Admin Login System and an Admin Dashboard `/admin`
+
+# v1.0
+
+1. Website Only
+2. Has a fully functional website for both SFW and NSFW content4
--- gifgang.py
+++ gifgang.py
@@ -3,29 +3,29 @@ import os
 import discord
 from discord.ext import commands
 
-from src.db import Config
-from src.utils import Filenames
+from discordbot.db import Config
+from discordbot.utils import Filenames
 
 with open(Filenames.token_txt, "r", encoding="utf-8") as _token_file:
     TOKEN = _token_file.read()
 
 client = commands.Bot(command_prefix=Config.prefix)
 
-for filename in os.listdir('./src/cogs'):
+for filename in os.listdir('./discordbot/cogs'):
     if filename.endswith('.py'):
         try:
-            client.load_extension(f'src.cogs.{filename[:-3]}')
-            print(f"[+] Loaded: src.cogs.{filename[:-3]}")
+            client.load_extension(f'discordbot.cogs.{filename[:-3]}')
+            print(f"[+] Loaded: discordbot.cogs.{filename[:-3]}")
         except Exception as excl:
             print(
-                f"[+] Unable to load: src.cogs.{filename[:-3]}  :  {excl}")
+                f"[+] Unable to load: discordbot.cogs.{filename[:-3]}  :  {excl}")
 
 
 @client.command()
 async def loadex(ctx, extension):
     if ctx.author.id in Config.admins:
         client.load_extension(
-            f'src.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
+            f'discordbot.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
         await ctx.send(f"Loaded cog: {extension}")
     else:
         await ctx.send("You do not have permissions to use this command!")
@@ -35,7 +35,7 @@ async def loadex(ctx, extension):
 async def unloadex(ctx, extension):
     if ctx.author.id in Config.admins:
         client.unload_extension(
-            f'src.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
+            f'discordbot.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
         await ctx.send(f"Un-Loaded cog: {extension}")
     else:
         await ctx.send("You do not have permissions to use this command!")
