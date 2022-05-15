aeca2f9800ce58683f84d29a7180f492a74c45ba
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-15 19:21:28 +0530

    giphy is now completed

--- discordbot/cogs/Sfw.py
+++ discordbot/cogs/Sfw.py
@@ -11,26 +11,39 @@ class Sfw(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def giphy(self, ctx, mode, **query):
+    async def giphy(self, ctx, mode=None, *query):
+
+        all_modes = ("random", "trending", "search")
+        if mode is None:
+            mode = "random"
+        else:
+            if not(mode in all_modes):
+                mode = "random"
 
         giphy = sfw.Giphy(api_key=str(Important.giphy_api_key))
 
-        embed = discord.Embed(
-            title=f"Uptime",
-            color=0xff0000,
-            timestamp=datetime.utcnow()
-        )
-        embed.set_author(
-            name=str(self.client.user.name),
-            icon_url=str(self.client.user.avatar_url)
-        )
-        embed.add_field(
-            name="Website",
-            value=f"Status Code:",
-            inline=False
-        )
-        embed.set_footer(text=f"Reuqested by {ctx.author.name}")
-        await ctx.send(embed=embed)
+        if mode == "trending":
+            images_list = giphy.trending(limit=1)
+        elif mode == "search":
+            images_list = giphy.search(query=' '.join(query), limit=1)
+        else:
+            images_list = giphy.random(limit=1)
+
+        for image in images_list:
+            embed = discord.Embed(
+                title=image['title'],
+                color=0xff0000,
+                timestamp=datetime.utcnow()
+            )
+            embed.set_author(
+                name=str(self.client.user.name),
+                icon_url=str(self.client.user.avatar_url)
+            )
+            embed.set_image(url=str(image['url']))
+            embed.set_footer(text=f"GIPHY - Reuqested by {ctx.author.name}")
+            await ctx.send(embed=embed)
+
+        del(giphy)
 
 
 def setup(client: commands.Bot):
