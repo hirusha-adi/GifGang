de99e781b66b3978546bdd5f0f26362ebaf1f03f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-15 16:05:46 +0530

    file location updat

--- discord-bot/src/utils.py
+++ discord-bot/src/utils.py
@@ -3,5 +3,5 @@ import os
 
 class Filenames:
     _cwd = os.getcwd()
-    config_json = os.path.join(_cwd, "database", "config.json")
+    config_json = os.path.join(_cwd, "src", "config.json")
     token_txt = os.path.join(_cwd, "token.txt")
