196218a604b1b44a6bba5c584dd11495f72b7b1f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-05-15 16:05:35 +0530

    access variables from config.json

--- discord-bot/src/db.py
+++ discord-bot/src/db.py
@@ -1,8 +1,14 @@
-import os
 import json
+import os
+
 from src.utils import Filenames
 
 
 class Config:
     with open(Filenames.config_json, "r", encoding="utf-8") as _file:
         data = json.load(_file)
+
+    prefix = data["prefix"]
+    admins = data["admins"]
+    channels = data["channels"]
+    channels_nsfw = channels["nsfw"]
